73f1d9b138bf30ea073eb432cb39badd66b43090
Tushar Shah <tushar.shah1@gmail.com>
2022-09-21 09:34:31 -0400

    updated step1 and 2

--- labs/aws/in-person/typescript/lab-02/code/step2.ts
+++ labs/aws/in-person/typescript/lab-02/code/step2.ts
@@ -5,7 +5,7 @@ const myami = aws.ec2.getAmi({
     filters: [{ name: "name", values: ["amzn2-ami-k*-hvm-*-x86_64-gp2"] }],
     owners: [ "amazon" ],
     mostRecent: true,
-})
+});
 
 // Exporting AMI_ID
-export const ami_id = myami;
+export const ami_id = myami.then(ami=>ami.id);
